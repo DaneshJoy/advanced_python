47d89719812e719e4ed755519e859f7d782a4a11
Saeed <DaneshJoy.ir@gmail.com>
2025-09-17 15:44:26 +0330

    added slide 1 + changes in codes

--- programming_paradigms/main.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-from signal_ops.signal_utils import make_signal
-from visualizations.vis_utils import visualize_signal
-
-x, y = make_signal('sin', (0, 2 * np.pi))
-visualize_signal(x, y, 'Sine Wave')
